5294dd4b00d6efcc60e4f09405b73313d002e240
shahidpatel88 <shahid.patel56@gmail.com>
2023-05-18 14:59:58 +0530

    Added Question Model & Migrations

---
+++ app/Models/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['title','slug','body','views','answers','votes','best_answer_id','user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=Str::slug($value);
+
+    }
+}
